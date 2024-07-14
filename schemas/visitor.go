13eff0e2b9da4d8c59a49f72588446facd2ed29a
João Pedro Maciel <146394493+JoaoPedr0Maciel@users.noreply.github.com>
2024-07-13 20:14:26 -0400

    refactor: code identity

--- schemas/visitor.go
+++ schemas/visitor.go
@@ -5,12 +5,12 @@ import (
 )
 
 type Visitor struct {
-	ID           uint      `json:"id"`
-	Name         string    `json:"name"`
-	Sexo         string    `json:"sexo"`
-	Cpf          string    `json:"cpf"`
-	HouseToVisit int64     `json:"house_number"`
-	WhoAllowed int64 `json:"who_allowed_cpf"`
-	EntryTime    time.Time `json:"entry_time"`
+	ID           uint       `json:"id"`
+	Name         string     `json:"name"`
+	Sexo         string     `json:"sexo"`
+	Cpf          string     `json:"cpf"`
+	HouseToVisit int64      `json:"house_number"`
+	WhoAllowed   int64      `json:"who_allowed_cpf"`
+	EntryTime    time.Time  `json:"entry_time"`
 	ExitTime     *time.Time `json:"exit_time"`
 }
